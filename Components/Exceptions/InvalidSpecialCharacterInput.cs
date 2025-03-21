f6818e849642163dba42d40d5f18b673f35630fe
Santiago Pabon <santiago.pabon72@gmail.com>
2025-03-20 20:56:41 -0600

    Add project files.

---
+++ Components/Exceptions/InvalidSpecialCharacterInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridApp.Components.Exceptions
+{
+    internal class InvalidSpecialCharacterInput : Exception
+    {
+        public InvalidSpecialCharacterInput(string errorMessage) : base(errorMessage)
+        {
+        }
+    }
+}
