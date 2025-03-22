6b7c9120b4b20f0eac2401b58303683b1ca6a2a9
santiago pabon <santiago.pabon72@gmail.com>
2025-03-22 15:34:13 -0600

    final commit includes all comments for code

--- Components/Data/TodoItem.cs
+++ Components/Data/TodoItem.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Author:Santiago Pabon Date: 2025 - 03- 22. This is the todoItem class.
+
 namespace BlazorHybridApp.Components.Data
 {
     public class TodoItem
--- Components/Exceptions/InvalidSpecialCharacterInput.cs
+++ Components/Exceptions/InvalidSpecialCharacterInput.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Author:Santiago Pabon Date: 2025 - 03- 22. This is the custom exception class that allows
+// for a InvalidSpecialCharacterInput to be executed.
+
+
+
 
 namespace BlazorHybridApp.Components.Exceptions
 {
